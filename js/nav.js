9e2c48db94d0268f8395c0e93368ae22e3d35ea7
Tim-Thiel <braunsilas7@gmail.com>
2026-04-08 13:45:10 +0200

    .

--- js/nav.js
+++ js/nav.js
@@ -9,11 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     favicon.href = '/bilder/thumbs/favicon.png';
 
-    // FontAwesome
-    const fa = document.createElement("link");
-    fa.rel  = "stylesheet";
-    fa.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css";
-    document.head.appendChild(fa);
 
 
     // Navigation
