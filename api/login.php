cfa6eb2530424fbd76b53e2c04e734ce75388150
Tim-Thiel <braunsilas7@gmail.com>
2026-08-30 18:16:45 +0200

    Lotti

--- api/login.php
+++ api/login.php
@@ -27,6 +27,7 @@ $PASSWORDS = [
     'aktionen' => '9b6fcb3d1877b41b17e0051e1ac4da83e1b20e9c91b73de5abb5189782f6160f',
     'team'     => '126d1337beb85580e514c90bfb75d92b90adb11feec2062ff01868e73f8444bb',
     'privat'   => '8fe9a4033f5d75198b568dd54c6af0824c5dbf00a3d2f9fdedebf2cd3bb2d3cf',
+    'lotti'    => '62fe4843ff9d2d79c52749cb0073c8490e7e490f03b98911d3acd4661ea69b5b',
 ];
 
 if (!isset($PASSWORDS[$area]) || !hash_equals($PASSWORDS[$area], hash('sha256', $password))) {
